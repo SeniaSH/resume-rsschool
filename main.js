427f0b88b5f3031270c7b885285a82b996248988
SeniaSH <arsen.shabanov.1973@mail.ru>
2022-02-22 18:33:45 +0300

    feat: add burger menu

--- main.js
+++ main.js
@@ -20,13 +20,34 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 const section = document.querySelectorAll('section[id]')
-function scrollAvtive(){
+function scrollActive(){
     const scrollY = window.pageYOffset
 
     section.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = cerrent.offsetTop - 50
-        sectionID = current.getAttribute('id')
-        
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+        }
     })
 }
+window.addEventListener('scroll', scrollActive)
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+function scrollHeader(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
